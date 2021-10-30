8865acb2fb4a2527a366334a0107cc6eed8d43b7
richrn <richrn@mail.uc.edu>
2021-10-30 04:53:23 -0400

    branch for input logging testing
    branch for input logging testing
    test.js
    additionally alternative p.jsn file

--- Template/src/views/Dashboard/Dashboard.js
+++ Template/src/views/Dashboard/Dashboard.js
@@ -1,33 +1,19 @@
 import React from "react";
-// react plugin for creating charts
-//import ChartistGraph from "react-chartist";
-// @material-ui/core
-//import { makeStyles } from "@material-ui/core/styles";
-//import Icon from "@material-ui/core/Icon";
-// @material-ui/icons
-//import Store from "@material-ui/icons/Store";
-//import Warning from "@material-ui/icons/Warning";
-//import DateRange from "@material-ui/icons/DateRange";
-//import LocalOffer from "@material-ui/icons/LocalOffer";
-//import Update from "@material-ui/icons/Update";
-//import ArrowUpward from "@material-ui/icons/ArrowUpward";
-//import AccessTime from "@material-ui/icons/AccessTime";
-//import Accessibility from "@material-ui/icons/Accessibility";
-//import BugReport from "@material-ui/icons/BugReport";
-//import Code from "@material-ui/icons/Code";
-//import Cloud from "@material-ui/icons/Cloud";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+//import InputLabel from "@material-ui/core/InputLabel";
 // core components
-//import GridItem from "components/Grid/GridItem.js";
-//import GridContainer from "components/Grid/GridContainer.js";
-//import Table from "components/Table/Table.js";
-//import Tasks from "components/Tasks/Tasks.js";
-//import CustomTabs from "components/CustomTabs/CustomTabs.js";
-//import Danger from "components/Typography/Danger.js";
-//import Card from "components/Card/Card.js";
-//import CardHeader from "components/Card/CardHeader.js";
-//import CardIcon from "components/Card/CardIcon.js";
-//import CardBody from "components/Card/CardBody.js";
-//import CardFooter from "components/Card/CardFooter.js";
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import CustomInput from "components/CustomInput/CustomInput.js";
+import Button from "components/CustomButtons/Button.js";
+import Card from "components/Card/Card.js";
+import CardHeader from "components/Card/CardHeader.js";
+//import CardAvatar from "components/Card/CardAvatar.js";
+import CardBody from "components/Card/CardBody.js";
+import CardFooter from "components/Card/CardFooter.js";
+
+//import avatar from "assets/img/faces/marc.jpg";
 
 //import { bugs, website, server } from "variables/general.js";
 
@@ -41,10 +27,83 @@ import React from "react";
 
 //const useStyles = makeStyles(styles);
 
-export default function Dashboard() {
+
 //  const classes = useStyles();
-  return (
-    <div> 
-    </div>
-  );
-}
+
+    const styles = {
+      cardCategoryWhite: {
+        color: "rgba(255,255,255,.62)",
+        margin: "0",
+        fontSize: "14px",
+        marginTop: "0",
+        marginBottom: "0",
+      },
+      cardTitleWhite: {
+        color: "#FFFFFF",
+        marginTop: "0px",
+        minHeight: "auto",
+        fontWeight: "300",
+        fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+        marginBottom: "3px",
+        textDecoration: "none",
+      },
+    };
+    
+    const useStyles = makeStyles(styles);
+    
+    export default function Dashboard() {
+      const classes = useStyles();
+
+      return (
+        <div>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={8}>
+              <Card>
+                <CardHeader color="primary">
+                  <h4 className={classes.cardTitleWhite}>Search for available storage</h4>
+                </CardHeader>
+                <CardBody>
+                  <GridContainer>
+                    <GridItem xs={12} sm={12} md={5}>
+                      <CustomInput
+                        labelText="Zipcode"
+                        id="Zipcode"
+                        formControlProps={{
+                          fullWidth: true,
+                          
+                        }}
+                      />
+                      
+                    </GridItem>
+                    <GridItem xs={12} sm={12} md={3}>
+                      <CustomInput
+                        labelText="Time needed"
+                        id="Time"
+                        formControlProps={{
+                          fullWidth: true,
+                        }}
+                      />
+                    </GridItem>
+                    <GridItem xs={12} sm={12} md={4}>
+                      <CustomInput
+                        labelText="Storage space"
+                        id="StorageSpace"
+                        formControlProps={{
+                          fullWidth: true,
+                        }}
+                      />
+                    </GridItem>
+                  </GridContainer>
+                </CardBody>
+                <CardFooter>
+               
+                  <Button color="primary" >Search</Button>
+                
+                </CardFooter>
+              </Card>
+            </GridItem>
+          </GridContainer>
+        </div>
+      );
+    }
+  
