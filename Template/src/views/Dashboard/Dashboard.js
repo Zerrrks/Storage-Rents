fa37c2eba3ede4ca22957901b414b3c0d51a6b4e
richrn <richrn@mail.uc.edu>
2021-11-09 16:36:46 -0500

    problem push
    big ol bobby

--- Template/src/views/Dashboard/Dashboard.js
+++ Template/src/views/Dashboard/Dashboard.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React from "react";
+// @material-ui/core components
+//import { makeStyles } from "@material-ui/core/styles";
+//import InputLabel from "@material-ui/core/InputLabel";
+// core components
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import CustomInput from "components/CustomInput/CustomInput.js";
+import Button from "components/CustomButtons/Button.js";
+import Card from "components/Card/Card.js";
+import CardHeader from "components/Card/CardHeader.js";
+//import CardAvatar from "components/Card/CardAvatar.js";
+import CardBody from "components/Card/CardBody.js";
+import CardFooter from "components/Card/CardFooter.js";
+
+//import avatar from "assets/img/faces/marc.jpg";
+
+//import { bugs, website, server } from "variables/general.js";
+
+//import {
+//  dailySalesChart,
+//  emailsSubscriptionChart,
+// completedTasksChart,
+//} from "variables/charts.js";
+
+//import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
+
+//const useStyles = makeStyles(styles);
 
 
+//  const classes = useStyles();
 
-export default class CustomerForm extends React.Component {
-  constructor(props) {
-    super(props);
 
     
+export default class Dashboard extends React.Component() {
+    
+  constructor(props) {
+    super(props);
+const event = ''    
     
     this.state = {
       customer: {
@@ -15,11 +45,9 @@ export default class CustomerForm extends React.Component {
         storageSpace: props.storageSpace
       }
     }
-
-    
   }
 
-  handleZipcodeChanged(event) {
+    handleZipcodeChanged(event) {
     var customer        = this.state.customer;
     customer.zipcode  = event.target.value;
 
@@ -40,39 +68,78 @@ export default class CustomerForm extends React.Component {
     this.setState({ customer: customer });
   }
 
-  handleButtonClicked() {
+  handleButtonClicked(Event) {
     console.log(this.state.customer);
   }
+  
+
+  
+
+  
+
+
 
   render() {
     return (
-      <div>
-        <label>
-          Zip code: 
-        </label>
-        <input type="text" value={this.state.customer.zipcode} onChange={this.handleZipcodeChanged.bind(this)}/>
-        <br/>
-        <label>
-          Last Name:
-        </label>
-        <input type="text" value={this.state.customer.timeNeeded} onChange={this.handleTimeNeededChanged.bind(this)}/>
-        <br/>
-        <label>
-          Status:
-        </label>
-        <select value={this.state.customer.storageSpace} onChange={this.handleStorageSpaceChanged.bind(this)}>
-          <option value="PENDING">
-            Pending
-          </option>
-          <option value="APPROVED">
-            Approved
-          </option>
-        </select>
-        <hr/>
-        <button onClick={this.handleButtonClicked.bind(this)}>
-          Search
-        </button>
-      </div>
-    );
+    <div>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={8}>
+              <Card>
+                <CardHeader color="primary">
+                  <h4 >Search for available storage</h4>
+                </CardHeader>
+                <CardBody>
+                  <GridContainer>
+                    <GridItem xs={12} sm={12} md={5}>
+                      <CustomInput
+                        labelText="Zipcode"
+                        id="zipcode"
+                        value={this.state.customer.zipcode}
+                        onChange={this.handleZipcodeChanged.bind(this)}
+                        formControlProps={{
+                          fullWidth: true,
+                          
+                        }}
+                      />
+                      
+                    </GridItem>
+                    <GridItem xs={12} sm={12} md={3}>
+                      <CustomInput
+                        labelText="Time needed"
+                        id="timeNeeded"
+                        value={this.state.customer.timeNeeded}
+                        onChange={this.handleTimeNeededChanged.bind(this)}
+                        formControlProps={{
+                          fullWidth: true,
+                        }}
+                      />
+                    </GridItem>
+                    <GridItem xs={12} sm={12} md={4}>
+                      <CustomInput
+                        labelText="Storage space"
+                        id="storageSpace"
+                        value={this.state.customer.storageSpace}
+                        onChange={this.handleStorageSpaceChanged.bind(this)}
+                        formControlProps={{
+                          fullWidth: true,
+                        }}
+                      />
+                    </GridItem>
+                  </GridContainer>
+                </CardBody>
+                <CardFooter>
+               
+                  <Button color="primary"
+                  onClick={this.handleButtonClicked.bind(this)}
+                   >Search</Button>
+                
+                </CardFooter>
+              </Card>
+            </GridItem>
+          </GridContainer>
+        </div>
+
+        
+      );
+    } 
   }
-}
---
+++ Template/src/views/Dashboard/test.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+
+
+export default class CustomerForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    
+    
+    this.state = {
+      customer: {
+        zipcode: props.zipcode,
+        timeNeeded: props.timeNeeded,
+        storageSpace: props.storageSpace
+      }
+    }
+
+    
+  }
+
+  handleZipcodeChanged(event) {
+    var customer        = this.state.customer;
+    customer.zipcode  = event.target.value;
+
+    this.setState({ customer: customer });
+  }
+
+  handleTimeNeededChanged(event) {
+    var customer      = this.state.customer;
+    customer.timeNeeded = event.target.value;
+
+    this.setState({ customer: customer });
+  }
+
+  handleStorageSpaceChanged(event) {
+    var customer    = this.state.customer;
+    customer.storageSpace = event.target.value;
+
+    this.setState({ customer: customer });
+  }
+
+  handleButtonClicked() {
+    console.log(this.state.customer);
+  }
+
+  render() {
+    return (
+      <div>
+        <label>
+          Zip code: 
+        </label>
+        <input type="text" value={this.state.customer.zipcode} onChange={this.handleZipcodeChanged.bind(this)}/>
+        <br/>
+        <label>
+          Last Name:
+        </label>
+        <input type="text" value={this.state.customer.timeNeeded} onChange={this.handleTimeNeededChanged.bind(this)}/>
+        <br/>
+        <label>
+          Status:
+        </label>
+        <select value={this.state.customer.storageSpace} onChange={this.handleStorageSpaceChanged.bind(this)}>
+          <option value="PENDING">
+            Pending
+          </option>
+          <option value="APPROVED">
+            Approved
+          </option>
+        </select>
+        <hr/>
+        <button onClick={this.handleButtonClicked.bind(this)}>
+          Search
+        </button>
+      </div>
+    );
+  }
+}
