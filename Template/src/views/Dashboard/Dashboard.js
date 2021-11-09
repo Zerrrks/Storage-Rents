5f9a42d7a3d229cd3398e720314bcacec790699f
nbinsky <binskynp@mail.uc.edu>
2021-11-09 17:37:38 -0500

    Dashboard first step

--- Template/src/views/Dashboard/Dashboard.js
+++ Template/src/views/Dashboard/Dashboard.js
@@ -6,13 +6,13 @@ import { makeStyles } from "@material-ui/core/styles";
 import Icon from "@material-ui/core/Icon";
 // @material-ui/icons
 import Store from "@material-ui/icons/Store";
-import Warning from "@material-ui/icons/Warning";
-import DateRange from "@material-ui/icons/DateRange";
-import LocalOffer from "@material-ui/icons/LocalOffer";
-import Update from "@material-ui/icons/Update";
+//import Warning from "@material-ui/icons/Warning";
+//import DateRange from "@material-ui/icons/DateRange";
+//import LocalOffer from "@material-ui/icons/LocalOffer";
+//import Update from "@material-ui/icons/Update";
 //import ArrowUpward from "@material-ui/icons/ArrowUpward";
 //import AccessTime from "@material-ui/icons/AccessTime";
-import Accessibility from "@material-ui/icons/Accessibility";
+//import Accessibility from "@material-ui/icons/Accessibility";
 //import BugReport from "@material-ui/icons/BugReport";
 //import Code from "@material-ui/icons/Code";
 //import Cloud from "@material-ui/icons/Cloud";
@@ -22,13 +22,13 @@ import GridContainer from "components/Grid/GridContainer.js";
 //import Table from "components/Table/Table.js";
 //import Tasks from "components/Tasks/Tasks.js";
 //import CustomTabs from "components/CustomTabs/CustomTabs.js";
-import Danger from "components/Typography/Danger.js";
+//import Danger from "components/Typography/Danger.js";
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CardIcon from "components/Card/CardIcon.js";
 //import CardBody from "components/Card/CardBody.js";
-import CardFooter from "components/Card/CardFooter.js";
-
+//import CardFooter from "components/Card/CardFooter.js";
+import logo from 'assets/img/srlogo.png'
 //import { bugs, website, server } from "variables/general.js";
 
 /*import {
@@ -46,82 +46,54 @@ export default function Dashboard() {
   return (
     <div>
       <GridContainer>
-        <GridItem xs={12} sm={6} md={3}>
+        <GridItem xs={6} sm={3} md={6}>
           <Card>
-            <CardHeader color="warning" stats icon>
-              <CardIcon color="warning">
+            <CardHeader color="rose" stats icon>
+              <CardIcon color="rose">
                 <Icon>content_copy</Icon>
               </CardIcon>
-              <p className={classes.cardCategory}>Used Space</p>
-              <h3 className={classes.cardTitle}>
-                49/50 <small>GB</small>
-              </h3>
+              <h1 className={classes.cardTitle}>Have something to store?</h1>
+              <h2 className={classes.cardTitle}>Login</h2>
+              <h2 className={classes.cardTitle}>Signup</h2>
             </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <Danger>
-                  <Warning />
-                </Danger>
-                <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                  Get more space
-                </a>
-              </div>
-            </CardFooter>
+            
           </Card>
         </GridItem>
-        <GridItem xs={12} sm={6} md={3}>
+        <GridItem xs={6} sm={2} md={6}>
           <Card>
             <CardHeader color="success" stats icon>
               <CardIcon color="success">
                 <Store />
               </CardIcon>
-              <p className={classes.cardCategory}>Revenue</p>
-              <h3 className={classes.cardTitle}>$34,245</h3>
+              <h1 className={classes.cardTitle}>Have something to store?</h1>
+              <h2 className={classes.cardTitle}>Login</h2>
+              <h2 className={classes.cardTitle}>Signup</h2>
             </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <DateRange />
-                Last 24 Hours
-              </div>
-            </CardFooter>
+
           </Card>
         </GridItem>
-        <GridItem xs={12} sm={6} md={3}>
+        <GridItem xs={10} sm={10} md={6}>
           <Card>
-            <CardHeader color="danger" stats icon>
-              <CardIcon color="danger">
+            <CardHeader color= "info" stats icon>
+              <CardIcon color="info">
                 <Icon>info_outline</Icon>
               </CardIcon>
-              <p className={classes.cardCategory}>Fixed Issues</p>
-              <h3 className={classes.cardTitle}>75</h3>
+              <h1 className={classes.cardTitle}>Want to view locations near you?</h1>
+              <h2 className={classes.cardTitle}>View Locations</h2>
             </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <LocalOffer />
-                Tracked from Github
-              </div>
-            </CardFooter>
           </Card>
         </GridItem>
-        <GridItem xs={12} sm={6} md={3}>
+        <GridItem xs={10} sm={10} md={6}>
           <Card>
-            <CardHeader color="info" stats icon>
+            <CardHeader color= "info" stats icon>
               <CardIcon color="info">
-                <Accessibility />
+                <Icon>info_outline</Icon>
               </CardIcon>
-              <p className={classes.cardCategory}>Followers</p>
-              <h3 className={classes.cardTitle}>+245</h3>
+              <img src={logo}></img>;
             </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <Update />
-                Just Updated
-              </div>
-            </CardFooter>
           </Card>
         </GridItem>
       </GridContainer>
-
     </div>
   );
 }
