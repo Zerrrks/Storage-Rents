c25f3aa00c98e6d8d2ea046a680a7ca8775085fa
MajesticBagel <bobbybath35@gmail.com>
2021-11-14 18:28:32 -0500

    Inputs
    Replaced custom inputs with regular inputs, restyled them via CSS. The inputs return values so should be good to go for backend connection?

--- Template/src/views/Dashboard/Dashboard.js
+++ Template/src/views/Dashboard/Dashboard.js
@@ -1,100 +1,125 @@
 import React from "react";
-// react plugin for creating charts
-//import ChartistGraph from "react-chartist";
-// @material-ui/core
-import { makeStyles } from "@material-ui/core/styles";
-import Icon from "@material-ui/core/Icon";
-// @material-ui/icons
-import Store from "@material-ui/icons/Store";
-//import Warning from "@material-ui/icons/Warning";
-//import DateRange from "@material-ui/icons/DateRange";
-//import LocalOffer from "@material-ui/icons/LocalOffer";
-//import Update from "@material-ui/icons/Update";
-//import ArrowUpward from "@material-ui/icons/ArrowUpward";
-//import AccessTime from "@material-ui/icons/AccessTime";
-//import Accessibility from "@material-ui/icons/Accessibility";
-//import BugReport from "@material-ui/icons/BugReport";
-//import Code from "@material-ui/icons/Code";
-//import Cloud from "@material-ui/icons/Cloud";
+
+// @material-ui/core components
+//import { makeStyles } from "@material-ui/core/styles";
+//import InputLabel from "@material-ui/core/InputLabel";
 // core components
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
-//import Table from "components/Table/Table.js";
-//import Tasks from "components/Tasks/Tasks.js";
-//import CustomTabs from "components/CustomTabs/CustomTabs.js";
-//import Danger from "components/Typography/Danger.js";
+//import CustomInput from "components/CustomInput/CustomInput.js";
+import Button from "components/CustomButtons/Button.js";
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
-import CardIcon from "components/Card/CardIcon.js";
-//import CardBody from "components/Card/CardBody.js";
-//import CardFooter from "components/Card/CardFooter.js";
-import logo from 'assets/img/srlogo.png'
+//import CardAvatar from "components/Card/CardAvatar.js";
+import CardBody from "components/Card/CardBody.js";
+import CardFooter from "components/Card/CardFooter.js";
+
+
+
+//import avatar from "assets/img/faces/marc.jpg";
+
 //import { bugs, website, server } from "variables/general.js";
 
-/*import {
-  dailySalesChart,
-  emailsSubscriptionChart,
-  completedTasksChart,
-} from "variables/charts.js";
-*/
-import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
+//import {
+//  dailySalesChart,
+//  emailsSubscriptionChart,
+// completedTasksChart,
+//} from "variables/charts.js";
 
-const useStyles = makeStyles(styles);
+//import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
 
-export default function Dashboard() {
-  const classes = useStyles();
-  return (
-    <div>
-      <GridContainer>
-        <GridItem xs={6} sm={3} md={6}>
-          <Card>
-            <CardHeader color="rose" stats icon>
-              <CardIcon color="rose">
-                <Icon>content_copy</Icon>
-              </CardIcon>
-              <h1 className={classes.cardTitle}>Have something to store?</h1>
-              <h2 className={classes.cardTitle}>Login</h2>
-              <h2 className={classes.cardTitle}>Signup</h2>
-            </CardHeader>
-            
-          </Card>
-        </GridItem>
-        <GridItem xs={6} sm={2} md={6}>
-          <Card>
-            <CardHeader color="success" stats icon>
-              <CardIcon color="success">
-                <Store />
-              </CardIcon>
-              <h1 className={classes.cardTitle}>Have something to store?</h1>
-              <h2 className={classes.cardTitle}>Login</h2>
-              <h2 className={classes.cardTitle}>Signup</h2>
-            </CardHeader>
+//const useStyles = makeStyles(styles);
 
-          </Card>
-        </GridItem>
-        <GridItem xs={10} sm={10} md={6}>
-          <Card>
-            <CardHeader color= "info" stats icon>
-              <CardIcon color="info">
-                <Icon>info_outline</Icon>
-              </CardIcon>
-              <h1 className={classes.cardTitle}>Want to view locations near you?</h1>
-              <h2 className={classes.cardTitle}>View Locations</h2>
-            </CardHeader>
-          </Card>
-        </GridItem>
-        <GridItem xs={10} sm={10} md={6}>
-          <Card>
-            <CardHeader color= "info" stats icon>
-              <CardIcon color="info">
-                <Icon>info_outline</Icon>
-              </CardIcon>
-              <img src={logo}></img>;
-            </CardHeader>
-          </Card>
-        </GridItem>
-      </GridContainer>
-    </div>
-  );
+
+//const classes = useStyles();
+
+
+
+var PropTypes = require('prop-types');
+    
+export default class Dashboard extends React.Component {
+
+  constructor(props) {
+    super(props);
+    
+ 
+
+    this.state = {
+      customer: {
+        zipcode: props.zipcode,
+        timeNeeded: props.timeNeeded,
+        storageSpace: props.storageSpace
+      }
+    }
+  }
+
+    handleZipcodeChanged(event) {
+    var customer        = this.state.customer;
+    customer.zipcode  = event.target.value;
+
+    this.setState({ customer: customer });
+  }
+
+  handleTimeNeededChanged(event) {
+    var customer      = this.state.customer;
+    customer.timeNeeded = event.target.value;
+
+    this.setState({ customer: customer });
+  }
+
+  handleStorageSpaceChanged(event) {
+    var customer    = this.state.customer;
+    customer.storageSpace = event.target.value;
+
+    this.setState({ customer: customer });
+  }
+
+  handleButtonClicked() {
+    console.log(this.state.customer);
+  }
+
+ 
+
+  render() {
+    return (
+    <div>       
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={8}>
+              <Card>
+                <CardHeader color="primary">
+                  <h4 >Search for available storage</h4>
+                </CardHeader>
+                <CardBody>
+                  <GridContainer>
+                    <GridItem xs={12} sm={12} md={4}>
+                    <input className="zipcode-input" placeholder="Zipcode" type="text" id="Zipcode" value={this.state.customer.zipcode} onChange={this.handleZipcodeChanged.bind(this)}/>
+                    </GridItem>         
+                    <GridItem xs={12} sm={12} md={4}>
+                    <input className="use-time-input" placeholder="Use Time" type="text" id="TimeNeeded" value={this.state.customer.timeNeeded} onChange={this.handleTimeNeededChanged.bind(this)}/>
+                    </GridItem>
+                    <GridItem xs={12} sm={12} md={4}>
+                    <input className="storage-space-input" placeholder="Storage Space" type="text" id="StorageSpace" value={this.state.customer.storageSpace} onChange={this.handleStorageSpaceChanged.bind(this)}/>
+                    </GridItem>
+                    </GridContainer>
+                </CardBody>
+                <CardFooter>
+               
+                  <Button color="primary"
+                  onClick={this.handleButtonClicked.bind(this)}
+                   >Search</Button>                
+                </CardFooter>
+              </Card>
+            </GridItem>
+          </GridContainer>
+        </div>
+
+        
+      );
+    } 
 }
 
+Dashboard.propTypes = {
+  zipcode: PropTypes.string,
+  timeNeeded: PropTypes.string,
+  storageSpace: PropTypes.string
+}
