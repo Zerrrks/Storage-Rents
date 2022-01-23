214ed7c038aa20fca2be3b99fecef2e8ccca7e1c
richrn <richrn@mail.uc.edu>
2022-01-22 20:21:03 -0500

    Minor login page stuff- no db
    Minor login page stuff- no db

--- server/db.js
+++ server/db.js
@@ -2,10 +2,13 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "user123",
+    password: "DragonBalrog1863",
     host: "localhost",
     port: 5432,
     database: "storage_rents"
 });
+
+pool.connect();
+console.log ("client connected ")
 
 module.exports = pool;
