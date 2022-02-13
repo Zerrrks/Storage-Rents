e1db76b55e08b231bf52e46ecbe4e179f47b3c7a
MajesticBagel <bobbybath35@gmail.com>
2022-02-13 16:10:20 -0500

    Minor Changes
    Just changing the User Storage page back to what it was before I cut it up

--- Template/src/views/UserStorage/DisplayStorage.js
+++ Template/src/views/UserStorage/DisplayStorage.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState, useEffect } from "react";
-//import { history } from "history";
+
   
 import Table from "components/Table/Table.js";
 import GridItem from "components/Grid/GridItem";
@@ -16,10 +16,8 @@ import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
 //import CardAvatar from "components/Card/CardAvatar.js";
 import CardBody from "components/Card/CardBody.js";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-//import { withRouter } from "react-router-dom/cjs/react-router-dom.min";
+//import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 //import CardFooter from "components/Card/CardFooter.js";
-//import storageFinder from "../../components/storageFinder";
 //const cors = require("cors");
 const DisplayStorage = () => {
    // useEffect(async () => {
@@ -29,7 +27,7 @@ const DisplayStorage = () => {
    //     }   catch (err) {}
   //  }, [])
     const [info, setInfo] = useState([]);
-    let history = useHistory
+  //  let history = useHistory
     async function getInfo() {
         const res = await fetch('http://localhost:5000/storage/1', {
             method: "GET"
@@ -41,9 +39,9 @@ const DisplayStorage = () => {
     useEffect(() => {
         getInfo();
     }, []);
-    const handleUpdate = (storage_id) => {
-        history.push(`/storage/${storage_id}/`);
-    };
+  //  const handleUpdate = (storage_id) => {
+  //      history.push(`/storage/${storage_id}/`);
+  //  };
     const styles = {
         cardCategoryWhite: {
             color: "rgba(255,255,255,.62)",
@@ -96,12 +94,7 @@ const DisplayStorage = () => {
                         </GridContainer>
                     </CardBody>
                 </Card>
-                <button onClick={() => handleUpdate(info.storage_id)}>
-         Edit Storage Details 
-        </button>
-            </GridItem>
-            <br></br>
-        <a href="/Admin/addstorage">Add Storage</a>
+            </GridItem>      
         </Fragment>
     );
 };
